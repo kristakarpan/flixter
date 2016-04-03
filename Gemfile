77a5b9e5bddb474daecf0291db41cd085290ce22
Krista Karpan <krista.karpan@gmail.com>
2016-04-03 15:01:36 +0000

    Add image uploader

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'devise', '>= 3.2.4'
 
 gem 'simple_form'
 
+gem 'carrierwave'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- db/migrate/20160327043304_create_courses.rb
+++ db/migrate/20160327043304_create_courses.rb
@@ -4,6 +4,7 @@ class CreateCourses < ActiveRecord::Migration
     	t.string :title
     	t.text :description
     	t.decimal :cost
+      t.string :image
     	t.integer :user_id
       t.timestamps
     end
