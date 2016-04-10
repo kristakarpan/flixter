763ccb0517a9caa1da8da7344e4e453802fbaba3
Krista Karpan <krista.karpan@gmail.com>
2016-04-10 15:39:51 +0000

    Add style to instructor show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Flixter::Application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
-      resources :courses, only: [:new, :create, :show] do
-        resources :sections, only: [:new, :create]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
