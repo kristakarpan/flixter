8833bcbad94f193850ac87b2155f71736a54ee7d
Krista Karpan <krista.karpan@gmail.com>
2016-05-01 21:00:11 +0000

    Clean up old migration file

--- db/migrate/20160327043304_create_courses.rb
+++ db/migrate/20160327043304_create_courses.rb
@@ -4,7 +4,6 @@ class CreateCourses < ActiveRecord::Migration
     	t.string :title
     	t.text :description
     	t.decimal :cost
-      t.string :image
     	t.integer :user_id
       t.timestamps
     end
